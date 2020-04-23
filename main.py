4e2860d8ae6dd37b5dbeb5a2fc29a4bd22459821
Aslak Hollund <aslakhol@gmail.com>
2020-04-23 14:27:49 +0200

    play episode

--- main.py
+++ main.py
@@ -15,32 +15,36 @@ class Agent:
         self.verbose = config["verbose"]
         self.policy = Policy(hex_config["size"] ** 2)
         self.stats = {1: 0, 2: 0}
+        self.states = []
+        self.distributions = []
 
     def play(self):
-        states = []
-        distributions = []
         for _ in range(1, self.episodes + 1):
-            game = Game()
-            mcst = MonteCarloSearchTree(config["M"], config["c"], policy=self.policy)
-            node = MonteCarloSearchNode(
-                is_root=True, game_object=game, parent=None, move_from_parent=None
-            )
-            while not game.is_end_state():
-                action, node = mcst.suggest_action(node)
-                current_player = game.current_player
-                states.append(node.game_object.get_state())
-                distribution = mcst.get_distribution(node)
-                distributions.append(distribution)
-                game.move(action, self.verbose)
-            self.stats[current_player] += 1
-            number_in_batch = len(states) // 3
-            states_batch, distributions_batch = zip(
-                *random.sample(
-                    list(zip(states, distributions)), number_in_batch
-                )  # Gives a random sample for training
-            )
-            self.policy.train_from_batch(states_batch, distributions_batch)
+            self.play_episode()
+
         self.display_result()
+
+    def play_episode(self):
+        game = Game()
+        mcst = MonteCarloSearchTree(config["M"], config["c"], policy=self.policy)
+        node = MonteCarloSearchNode(
+            is_root=True, game_object=game, parent=None, move_from_parent=None
+        )
+        while not game.is_end_state():
+            action, node = mcst.suggest_action(node)
+            current_player = game.current_player
+            self.states.append(node.game_object.get_state())
+            distribution = mcst.get_distribution(node)
+            self.distributions.append(distribution)
+            game.move(action, self.verbose)
+        self.stats[current_player] += 1
+        number_in_batch = len(self.states) // 3
+        states_batch, distributions_batch = zip(
+            *random.sample(
+                list(zip(self.states, self.distributions)), number_in_batch
+            )  # Gives a random sample for training
+        )
+        self.policy.train_from_batch(states_batch, distributions_batch)
 
     def display_result(self):
         print(
