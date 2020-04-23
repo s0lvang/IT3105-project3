c5ad2ce2560f7f296dba106d6a6a6e72bbd7b17c
Aslak Hollund <aslakhol@gmail.com>
2020-04-23 15:07:25 +0200

    episode to own file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from node import MonteCarloSearchNode
 from config import general as config
 from config import hex as hex_config
 from policy import Policy
+from episode import Episode
 import random
 
 
@@ -30,49 +31,6 @@ class Agent:
 
     def update_result(self, winner):
         self.stats[winner] += 1
-
-    def display_result(self):
-        print(
-            f"Player 1 won {self.stats[1]}/{self.episodes} ({round(100 * self.stats[1]/self.episodes, 1)}%)"
-        )
-        print(
-            f"Player 2 won {self.stats[2]}/{self.episodes} ({round(100 * self.stats[2]/self.episodes, 1)}%)"
-        )
-
-
-class Episode:
-    def __init__(self, policy, verbose):
-        self.game = Game()
-        self.mcst = MonteCarloSearchTree(config["M"], config["c"], policy=policy)
-        self.starting_node = MonteCarloSearchNode(
-            is_root=True, game_object=self.game, parent=None, move_from_parent=None
-        )
-        self.policy = policy
-        self.states = []
-        self.distributions = []
-        self.verbose = verbose
-
-    def play(self):
-        node = self.starting_node
-        while not self.game.is_end_state():
-            action, node = self.mcst.suggest_action(node)
-            current_player = self.game.current_player
-
-            state = node.game_object.get_state()
-            distribution = self.mcst.get_distribution(node)
-
-            self.states.append(state)
-            self.distributions.append(distribution)
-
-            self.game.move(action, self.verbose)
-
-        number_in_batch = len(self.states) // 3
-        states_batch, distributions_batch = zip(
-            *random.sample(
-                list(zip(self.states, self.distributions)), number_in_batch
-            )  # Gives a random sample for training
-        )
-        self.policy.train_from_batch(states_batch, distributions_batch)
 
     def display_result(self):
         print(
