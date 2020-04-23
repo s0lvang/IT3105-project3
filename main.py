643de317cd9c8affc5bf23b91b6be050a1ada627
Aslak Hollund <aslakhol@gmail.com>
2020-04-23 14:27:28 +0200

    remove tf logging

--- main.py
+++ main.py
@@ -25,22 +25,24 @@ class Agent:
             node = MonteCarloSearchNode(
                 is_root=True, game_object=game, parent=None, move_from_parent=None
             )
-            print(_)
             while not game.is_end_state():
                 action, node = mcst.suggest_action(node)
                 current_player = game.current_player
                 states.append(node.game_object.get_state())
                 distribution = mcst.get_distribution(node)
-                distributions.append(
-                    distribution
-                )
+                distributions.append(distribution)
                 game.move(action, self.verbose)
             self.stats[current_player] += 1
-            number_in_batch = len(states)//3
+            number_in_batch = len(states) // 3
             states_batch, distributions_batch = zip(
-                *random.sample(list(zip(states, distributions)), number_in_batch) #Gives a random sample for training
+                *random.sample(
+                    list(zip(states, distributions)), number_in_batch
+                )  # Gives a random sample for training
             )
             self.policy.train_from_batch(states_batch, distributions_batch)
+        self.display_result()
+
+    def display_result(self):
         print(
             f"Player 1 won {self.stats[1]}/{self.episodes} ({round(100 * self.stats[1]/self.episodes, 1)}%)"
         )
