0969b489807b568b59ff1a10c1d4eeb85186db3c
s0lvang <august.s.solvang@gmail.com>
2020-04-28 16:40:52 +0200

    adds a more complex config

--- config.py
+++ config.py
@@ -1,3 +1,43 @@
+from enum import Enum
+from tensorflow import keras
+
+
+class Activation(Enum):
+    SIGMOID = "sigmoid"
+    LINEAR = "linear"
+    TANH = "tanh"
+    RELU = "relu"
+    SOFTMAX = "softmax"
+
+
+lr = 0.01
+
+
+class Optmizers(Enum):
+    RMSProp = keras.optimizers.RMSprop(
+        learning_rate=lr,
+        rho=0.9,
+        momentum=0.0,
+        epsilon=1e-07,
+        centered=False,
+        name="RMSprop",
+    )
+    Adam = keras.optimizers.Adam(
+        learning_rate=lr,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=1e-07,
+        amsgrad=False,
+        name="Adam",
+    )
+    Adagrad = keras.optimizers.Adagrad(
+        learning_rate=lr, epsilon=1e-07, initial_accumulator_value=0.1, name="Adagrad"
+    )
+    SGD = keras.optimizers.SGD(
+        learning_rate=lr, momentum=0.0, nesterov=False, name="SGD"
+    )
+
+
 hex = dict(size=4)
 
 general = dict(
@@ -10,4 +50,10 @@ general = dict(
     rollouts=200,  # 300 rollouts
     epsilon=1,
     epsilon_decay_rate=0.05,
+)
+
+ANN = dict(
+    hidden_layers=[(30, Activation.SOFTMAX), (20, Activation.SOFTMAX)],
+    learning_rate=lr,
+    optimizer=Optmizers.Adagrad,
 )
