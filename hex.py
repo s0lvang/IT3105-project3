fc32d9ad258f352e848b15729232de7415b15307
Aslak Hollund <aslakhol@gmail.com>
2020-04-29 18:27:58 +0200

    first pass hex rewrite

--- hex.py
+++ hex.py
@@ -6,36 +6,37 @@ import math
 
 
 class Hex:
-    board = []
-
     def __init__(self, size, state=None):
-        self.board = [[Node(i, j, owner=0) for j in range(size)] for i in range(size)]
         if state:
-            count = 0
-            for i in range(len(self.board)):
-                for j in range(len(self.board[i])):
-                    self.board[i][j].owner = state[count]
-                    count += 1
-        self.size = size
-        self.set_all_neighbours(self.board)
+            self.board = state
+        else:
+            self.board = self.setup_board(size)
 
-    def position_is_on_board(self, r, c):
-        return 0 <= r and len(self.board) > r and 0 <= c and len(self.board[r]) > c
+        self.size = size
+
+    def setup_board(self, size):
+        slot = (0, 0)  # node[0]=player1, node[1]=player2
+        return [slot] * (size ** 2)
 
     def get_legal_moves(self):
-        legalMoves = []
-        for row in self.board:
-            for node in row:
-                if not node.owner:
-                    legalMoves.append(node.coordinates)
-        return legalMoves
+        board = self.board
+        legal_moves = []
+        for i in board:
+            slot = board[i]
+            if slot[0] == 0 and slot[1] == 0:
+                legal_moves.append(i)
 
-    def set_all_neighbours(self, board):
-        for row in board:
-            for node in row:
-                self.set_neighbours(node)
+        return legal_moves
 
-    def get_action_from_network_output(self, output):
+    def get_2d_coords(self, index, size):
+        return (index // size, index % size)
+
+    def get_1d_index(self, coords, size):
+        x = coords[0]
+        y = coords[1]
+        return y + (x * size)
+
+    def get_action_from_network_output(self, output):  # not done
         action = None
         output = output[0]
         while not self.is_legal_action(action):
@@ -47,89 +48,102 @@ class Hex:
             output[best_index] = -math.inf
         return action
 
-    def is_legal_action(self, action):
-        return (
-            action
-            and self.position_is_on_board(*action)
-            and not self.get_node_from_coordinates(action).owner
-        )
+    def is_legal_action(self, action):  # done
+        board = self.board
+        slot = board[action]
 
-    def set_neighbours(self, node):
-        neighbours = {
-            Direction.UP: self.up_neighbour(node),
-            Direction.UPRIGHT: self.up_right_neighbour(node),
-            Direction.RIGHT: self.right_neighbour(node),
-            Direction.DOWN: self.down_neighbour(node),
-            Direction.DOWNLEFT: self.down_left_neighbour(node),
-            Direction.LEFT: self.left_neighbour(node),
-        }
-        node.set_neighbours(neighbours)
+        is_on_board = 0 <= action and action < len(board)
+        is_filled = slot[0] == 1 or slot[1] == 1
+
+        return is_on_board and not is_filled
 
     def is_end_state(self):
-        self.set_nodes_unvisted()
-        return self.is_player1_winning() or self.is_player2_winning()
+        visited = [] * (self.size ** 2)
+        return self.is_player1_winning(visited) or self.is_player2_winning(visited)
 
-    def set_nodes_unvisted(self):
-        for row in self.board:
-            for node in row:
-                node.visited = False
+    def is_player1_winning(self, visited):
+        board = self.board
+        size = self.size
+        search_for_other_edge = self.search_for_other_edge
+        is_bottom_right = self.is_bottom_right
+        owner = 0
 
-    def is_player1_winning(self):
-        for i in range(self.size):
-            start_node = self.board[i][0]
-
-            if not start_node.owner == 1:
+        for i in range(0, size ** 2, size):
+            if not board[i][owner] == 1:
                 continue
 
-            if self.search_for_other_edge(start_node, start_node, self.is_bottom_right):
+            if search_for_other_edge(i, is_bottom_right, visited, owner):
                 return True
 
-    def is_player2_winning(self):
-        for i in range(self.size):
-            start_node = self.board[0][i]
+    def is_player2_winning(self, visited):
+        board = self.board
+        size = self.size
+        search_for_other_edge = self.search_for_other_edge
+        is_bottom_left = self.is_bottom_right
+        owner = 1
 
-            if not start_node.owner == 2:
+        for i in range(size):
+            if not board[i][owner] == 1:
                 continue
 
-            if self.search_for_other_edge(start_node, start_node, self.is_bottom_left):
+            if search_for_other_edge(i, is_bottom_left, visited, owner):
                 return True
 
-    def search_for_other_edge(self, node, initial_node, is_correct_edge):
-        node.visited = True
+    def search_for_other_edge(self, i, is_correct_edge, visited, owner):
+        visited[i] = True
+        board = self.board
+        search = self.search_for_other_edge
 
-        if is_correct_edge(node):
+        if is_correct_edge(i):
             return True
 
-        connected_neighbours = node.get_connected_neighbours()
-        unvisited_neighbours = filter(
-            lambda node: not node.visited, connected_neighbours
-        )
-        for neighbour in unvisited_neighbours:
-            if self.search_for_other_edge(neighbour, initial_node, is_correct_edge):
+        neighbours = self.get_neighbours(i)
+        connected = [x for x in neighbours if board[x][owner] == 1]
+        unvisited = [y for y in connected if visited[y] == 0]
+
+        for neighbour in unvisited:
+            if search(neighbour, is_correct_edge, visited, owner):
                 return True
 
-    def is_top_right(self, node):
-        return node.coordinates[0] == 0
+    def get_neighbours(self, i):
+        size = self.size
+        neighbours = []
 
-    def is_top_left(self, node):
-        return node.coordinates[1] == 0
+        if i - size >= 0:
+            neighbours.append(i - size)  # up
+        if i % size != 0:
+            neighbours.append(i - 1)  # left
+        if (i + 1) % size != 0:
+            neighbours.append(i + 1)  # right
+        if i + size < size ** 2:
+            neighbours.append(i + size)  # down
+        if (i - size + 1) >= 0 and ((i + 1) % size != 0):
+            neighbours.append(i - size + 1)  # up_right
+        if (i + size - 1) < size ** 2 and i % size != 0:
+            neighbours.append(i + size - 1)  # down_left
 
-    def is_bottom_right(self, node):
-        return node.coordinates[1] == self.size - 1
+        return neighbours
 
-    def is_bottom_left(self, node):
-        return node.coordinates[0] == self.size - 1
+    def is_bottom_right(self, i, size):
+        return i in range(size - 1, size ** 2, size)
+
+    def is_bottom_left(self, i, size):
+        return i in range((size ** 2) - size, size ** 2)
 
     def get_state(self):
-        return [node.owner for row in self.board for node in row]
+        return self.board
 
     def move(self, action, current_player):
-        if action:
-            node = self.get_node_from_coordinates(action)
-            if not node.owner:
-                node.owner = current_player
+        if self.is_legal_action(action):
+            if current_player == 1:
+                slot = (1, 0)
             else:
-                raise Exception("Move is not legal")
+                slot = (0, 1)
+
+            self.board[action] = slot
+        else:
+            raise Exception("Move is not legal")
+
         return self.is_end_state()
 
     def reward(self):
@@ -140,39 +154,6 @@ class Hex:
     def draw(self):
         Drawer().draw(self.board)
 
-    def get_node_from_coordinates(self, coordinates):
-        return self.board[coordinates[0]][coordinates[1]]
 
-    def up_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r - 1, c)
-
-    def up_right_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r - 1, c + 1)
-
-    def right_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r, c + 1)
-
-    def down_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r + 1, c)
-
-    def down_left_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r + 1, c - 1)
-
-    def left_neighbour(self, node):
-        r = node.coordinates[0]
-        c = node.coordinates[1]
-        return self.neighbour(r, c - 1)
-
-    def neighbour(self, r, c):
-        if self.position_is_on_board(r, c):
-            return self.board[r][c]
+if __name__ == "__main__":
+    hex = Hex(4)
