5de8274c9d30c48081662f6af2cdc01332dc6cab
Aslak Hollund <aslakhol@gmail.com>
2020-04-30 11:25:50 +0200

    setup ready

--- config.py
+++ config.py
@@ -7,7 +7,22 @@ general = dict(
     amount_of_players=10,
     games_in_series=2,
     c=1,
-    rollouts=75,  # 300 rollouts
+    rollouts=75,
     epsilon=1,
     draw=False,
 )
+
+
+# hex = dict(size=4)
+
+# general = dict(
+#     verbose=True,
+#     starting_player="one",  # one, two or mix
+#     episodes=50,
+#     amount_of_players=10,
+#     games_in_series=2,
+#     c=1,
+#     rollouts=50,
+#     epsilon=1,
+#     draw=False,
+# )
